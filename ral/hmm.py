b32aba9497a669731c686c94fe27efcc376aff69
yuanmingqi <myatcam2024@gmail.com>
2024-04-18 17:06:29 +0100

    u

---
+++ ral/hmm.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+num_joints = 21
+num_classes = 3
+
+class HandGestureModel(nn.Module):
+    def __init__(self, num_joints, num_classes, hidden_dim, num_layers):
+        super(HandGestureModel, self).__init__()
+        self.num_joints = num_joints
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(input_size=num_joints * 3, hidden_size=hidden_dim, 
+                            num_layers=num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_dim*2, num_classes)
+
+    def forward(self, x):
+        # x should be of shape (batch, sequence, features)
+        lstm_out, _ = self.lstm(x)
+        # Take the output of the last time step
+        last_time_step_out = lstm_out[:, -1, :]
+        out = self.fc(last_time_step_out)
+        return out
+
+class SequenceHandDataset(Dataset):
+    def __init__(self, sequences, labels):
+        self.sequences = sequences
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        # sequences[idx] should be of shape (sequence_length, num_joints * 3)
+        return torch.tensor(self.sequences[idx], dtype=torch.float32), self.labels[idx]
---
+++ ral/hmm_train.py
@@ -0,0 +1,39 @@
+from ral.hmm import SequenceHandDataset, HandGestureModel, num_classes, num_joints
+from torch.utils.data import DataLoader
+from torch import nn
+import numpy as np
+import torch
+
+data = np.load('./datasets/motion_datasets.npz')
+sequences, labels = data['sequences'], data['labels']
+print(sequences.shape, labels.shape)
+
+# Initialize the model with LSTM parameters
+hidden_dim = 64
+num_layers = 2
+batch_size = 15
+learning_rate = 0.001
+epochs = 1000
+
+dataset = SequenceHandDataset(sequences, labels)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+def train_model(model, dataloader, epochs):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    model.train()
+    for epoch in range(epochs):
+        for data, target in dataloader:
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+
+model = HandGestureModel(num_joints, num_classes, hidden_dim, num_layers)
+
+# Train the model
+train_model(model, dataloader, epochs=epochs)
+torch.save(model.state_dict(), './models/hmm.pth')
